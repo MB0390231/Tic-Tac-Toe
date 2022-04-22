3d2cc90b22e6082f0395c49f3ccf008de476d881
Michael <michaelbroyles68@gmail.com>
2022-04-21 21:09:04 -0500

    Converting to modules and factory functions

--- javascript.js
+++ javascript.js
@@ -1,35 +1,64 @@
 (function () {
     let content = document.querySelector('.content')
-    let gameboard = [
-        //connects the html elements
-        ["","","",],
-        ["","","",],
-        ["","","",]
-    ];
-    const person = (name,letter) => {
-        return {name,letter}
+    let players = [];
+    const player = (name,playerNumber) => {
+        let turn = () => {if (playerNumber == 'player1') {return true}}
+        
+        return ({name,playerNumber,turn})
     };
+
+    const gameboard = (() => {
+        /*gameboard refers to the html board 
+        board refers to the javascript board*/
+        let board = [
+            ["","","",],
+            ["","","",],
+            ["","","",]
+        ];
+        const bindGameboard = ()=>{
+            content.querySelectorAll('.square').forEach(square => {
+                square.addEventListener('click', function () {updateGameboard(square)})
+            });
+        };
+        const updateGameboard = ((square) => {
+            square.classList.add("played")
+            updateBoard(square,'X')
+        });
+        const updateBoard = ((element,letter) =>{
+            let column = element.getAttribute('data-position');
+            let row = element.parentElement.getAttribute('id');
+            board[row][column] = letter;
+        })
+        bindGameboard()
+        return board
+    })();
+
+
     function bindEvents(){   
         //used to bind events
-        content.querySelectorAll('.square').forEach(square => {
-            square.addEventListener('click', function () {updateBoard(square)})
-        });
         content.querySelectorAll('button').forEach(button => button.addEventListener('click', function(){bindPlayer(button)}))
-    };
-    function updateBoard (square) {
-        square.classList.add("played")
-        updateBoard(square.getAttribute('data-position'),player)
-        // updateBoard(square)
-    };
-    function updateBoard(index) {
-        //keeps the items in the gameboard array in line with the html elements
-        gameboard[index] = player.letter;
-
     };
     function bindPlayer(button) {
         //locks the player input field
-        content.querySelector(`#${button.className}`).readOnly = true;
+        let player = content.querySelector(`#${button.className}`);
+        player.readOnly = true;
+        player.style.border = 'none';
         button.style.backgroundColor = 'lightgreen';
+        
     };
     bindEvents();
-})()
+})();
+
+// const calculator = (() => {
+//     const add = (a, b) => a + b;
+//     const sub = (a, b) => a - b;
+//     const mul = (a, b) => a * b;
+//     const div = (a, b) => a / b;
+//     return {
+//       add,
+//       sub,
+//       mul,
+//       div,
+//     };
+//   })();
+  
