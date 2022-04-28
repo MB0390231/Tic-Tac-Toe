31fcc844a40ee00867431d4643f7959d01950aec
Michael Broyles <michaelbroyles68@gmail.com>
2022-04-28 18:28:59 -0500

    I had to convert everything to factory functions and modules (I thought I had but I hadn't and things got mesy). This is the bare bones first working version.

--- javascript.js
+++ javascript.js
@@ -1,64 +1,152 @@
-(function () {
-    let content = document.querySelector('.content')
-    let players = [];
-    const player = (name,playerNumber) => {
-        let turn = () => {if (playerNumber == 'player1') {return true}}
-        
-        return ({name,playerNumber,turn})
-    };
-
-    const gameboard = (() => {
-        /*gameboard refers to the html board 
-        board refers to the javascript board*/
-        let board = [
-            ["","","",],
-            ["","","",],
-            ["","","",]
-        ];
-        const bindGameboard = ()=>{
-            content.querySelectorAll('.square').forEach(square => {
-                square.addEventListener('click', function () {updateGameboard(square)})
-            });
+const game = (() => {
+    let jsBoard = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ];
+    const checkWin = (letter) => {
+        let checkRows = () => {
+            for(let r=0; r<3; r++) {
+                let count = 0;
+                for (let c=0; c<3; c++) {
+                    if (jsBoard[r][c]==letter) {
+                        count++;
+                    }
+                }
+                if (count == 3) {return true}
+            }
+            return false
         };
-        const updateGameboard = ((square) => {
-            square.classList.add("played")
-            updateBoard(square,'X')
-        });
-        const updateBoard = ((element,letter) =>{
-            let column = element.getAttribute('data-position');
-            let row = element.parentElement.getAttribute('id');
-            board[row][column] = letter;
-        })
-        bindGameboard()
-        return board
-    })();
-
-
-    function bindEvents(){   
-        //used to bind events
-        content.querySelectorAll('button').forEach(button => button.addEventListener('click', function(){bindPlayer(button)}))
+        let checkColumns = () => {
+            for (let c=0;c<3;c++) {
+                let count = 0;
+                for (let r=0; r<3;r++) {
+                    if (jsBoard[r][c] == letter ) {
+                        count++;
+                    } 
+                }
+                if (count==3) {return true}
+            }
+            return false
+        };
+        let checkDiagonals = () => {
+            if (jsBoard[0][0]==letter && jsBoard[1][1]==letter && jsBoard[2][2]==letter) {
+                return true
+            } if (jsBoard[0][2]==letter && jsBoard[1][1]==letter && jsBoard[2][0]==letter) {
+                return true
+            }
+            return false
     };
-    function bindPlayer(button) {
-        //locks the player input field
-        let player = content.querySelector(`#${button.className}`);
-        player.readOnly = true;
-        player.style.border = 'none';
-        button.style.backgroundColor = 'lightgreen';
-        
+    return checkRows() || checkColumns() || checkDiagonals()
     };
-    bindEvents();
+    const play = (letter,row,column) => {
+        jsBoard[row][column] = letter;
+        return
+    };
+    const reset = () => {
+        for (let r=0;r<3;r++) {
+            for (let c=0;c<3;c++) {
+                jsBoard[r][c] = "";
+            }
+        }
+        return
+    };
+    return {checkWin,play,reset}
 })();
 
-// const calculator = (() => {
-//     const add = (a, b) => a + b;
-//     const sub = (a, b) => a - b;
-//     const mul = (a, b) => a * b;
-//     const div = (a, b) => a / b;
-//     return {
-//       add,
-//       sub,
-//       mul,
-//       div,
-//     };
-//   })();
-  
+
+
+const controller = (() => {
+    const content = document.querySelector('.content').children;
+    const board = content[2];
+    const player1 = content[0];
+    const player2 = content[4];
+    const ready = document.querySelector('.ready');
+    const reset = document.querySelector('.reset')
+    const player = (name,letter) => {
+        const getName = () => name;
+        const getLetter = () => letter;
+        return {getName,getLetter}
+    }
+    players = [];
+    //push players
+    ready.addEventListener('click', function() {
+        if (player1.value == '' || player2.value == '') {return window.alert('Must fill out players names!')}
+        players.push(player(player1.value,'X'));
+        players.push(player(player2.value,'O'));
+        lockPlayers();
+        generateBoard();
+        flow();
+        return
+    });
+    reset.addEventListener('click',function() {resetEverything()})
+    const lockPlayers = () => {
+        //lock input
+        document.querySelectorAll('input').forEach(input => {
+            input.readOnly = true;
+            input.style.border = 'none';
+            input.style.fontSize = '2rem';
+            input.style.width = 'auto'
+        });
+        document.querySelectorAll('p').forEach(p =>{
+            p.style.fontSize = '2rem';
+        });
+        ready.remove();
+        return
+    };
+    const generateBoard = () => {
+        game.reset();
+        for (let r=0;r<3;r++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
+            row.id = r;
+            for (let c=0;c<3;c++) {
+                let square = document.createElement('div');
+                square.classList.add('square');
+                square.setAttribute('column',c)
+                row.appendChild(square)
+            }
+            board.appendChild(row)
+        }
+        return
+    };
+    const flow = () => {
+        count = 0;
+        const squares = document.querySelectorAll('.square');
+        squares.forEach(square => square.addEventListener('click',function () {
+            fillSquare(square,squares);
+        }))
+    };
+    const fillSquare = (square) => {
+        let row = square.parentNode.id;
+        let column = square.getAttribute('column');
+        index = count%2;
+        currentPlayer = players[index]
+        if (square.getAttribute('played') == 'true' || game.checkWin(currentPlayer.getLetter())) {return}
+            game.play(currentPlayer.getLetter(),row,column);
+            square.setAttribute('played','true')
+            square.innerHTML = currentPlayer.getLetter();
+            if (game.checkWin(currentPlayer.getLetter())) {
+                displayWinner(currentPlayer.getName())
+                return
+            }
+            count++;
+    };
+    const resetEverything = () =>{
+        let board = document.querySelector('.board');
+            while (board.firstChild) {
+            board.removeChild(board.firstChild);
+            };
+        document.querySelector('.winner').remove()
+        generateBoard();
+        flow();
+    };
+    const displayWinner = (name) => {
+        let winner = document.createElement('div');
+        winner.classList = 'winner';
+        winner.textContent = name + ' has won!';
+        document.querySelector('body').appendChild(winner);
+    }
+    return 
+})();
+
