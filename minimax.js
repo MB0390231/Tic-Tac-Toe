a9c2138c90cb14015c66f311ff280e9084c62a37
Michael Broyles <michaelbroyles68@gmail.com>
2022-04-30 18:30:58 -0500

    starting minimax

--- minimax.js
+++ minimax.js
@@ -64,3 +64,23 @@ const game = (() => {
     return {checkWin,play,reset,tie}
 })();
 
+
+
+// function minimax(board, depth, isMaximizingPlayer):
+
+//     if current board state is a terminal state :
+//         return value of the board
+    
+//     if isMaximizingPlayer :
+//         bestVal = -INFINITY 
+//         for each move in board :
+//             value = minimax(board, depth+1, false)
+//             bestVal = max( bestVal, value) 
+//         return bestVal
+
+//     else :
+//         bestVal = +INFINITY 
+//         for each move in board :
+//             value = minimax(board, depth+1, true)
+//             bestVal = min( bestVal, value) 
+//         return bestVal 
