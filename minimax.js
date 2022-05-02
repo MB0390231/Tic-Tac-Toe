3dc10f22df7c4539d7862ab8abe0bc75b79dd9b1
Michael Broyles <michaelbroyles68@gmail.com>
2022-05-02 15:27:46 -0500

    algorithm starting to take shape

--- minimax.js
+++ minimax.js
@@ -1,15 +1,10 @@
-const game = (() => {
-    let jsBoard = [
-        ['','',''],
-        ['','',''],
-        ['','','']
-    ];
+const game = (board) => {
     const checkWin = (letter) => {
         let checkRows = () => {
             for(let r=0; r<3; r++) {
                 let count = 0;
                 for (let c=0; c<3; c++) {
-                    if (jsBoard[r][c]==letter) {
+                    if (board[r][c]==letter) {
                         count++;
                     }
                 }
@@ -21,7 +16,7 @@ const game = (() => {
             for (let c=0;c<3;c++) {
                 let count = 0;
                 for (let r=0; r<3;r++) {
-                    if (jsBoard[r][c] == letter ) {
+                    if (board[r][c] == letter ) {
                         count++;
                     }
                 }
@@ -30,19 +25,19 @@ const game = (() => {
             return false
         };
         let checkDiagonals = () => {
-            if (jsBoard[0][0]==letter && jsBoard[1][1]==letter && jsBoard[2][2]==letter) {
+            if (board[0][0]==letter && board[1][1]==letter && board[2][2]==letter) {
                 return true
-            } if (jsBoard[0][2]==letter && jsBoard[1][1]==letter && jsBoard[2][0]==letter) {
+            } if (board[0][2]==letter && board[1][1]==letter && board[2][0]==letter) {
                 return true
             }
             return false
         };
-    return checkRows() || checkColumns() || checkDiagonals()
+    return checkRows(letter,board) || checkColumns(letter,board) || checkDiagonals(letter,board)
     };
-    const tie = () => {
+    const full = () => {
         for (let r=0;r<3;r++) {
             for (let c=0; c<3;c++) {
-                if (jsBoard[r][c] == '') {
+                if (board[r][c] == '') {
                     return false
                 }
             }
@@ -50,18 +45,18 @@ const game = (() => {
         return true
     };
     const play = (letter,row,column) => {
-        jsBoard[row][column] = letter;
+        board[row][column] = letter;
         return
     };
     const reset = () => {
         for (let r=0;r<3;r++) {
             for (let c=0;c<3;c++) {
-                jsBoard[r][c] = "";
+                board[r][c] = "";
             }
         }
         return
     };
-    const getMoves = (board) => {
+    const getMoves = () => {
         //need a way to return rows and columns in the board that are free
         let moves = [];
         for (let r=0;r<3;r++) {
@@ -71,24 +66,41 @@ const game = (() => {
         };
         return moves
     };
-    return {checkWin,play,reset,tie,getMoves}
-})();
 
-// function minimax(board, depth, isMaximizingPlayer):
+    const minimax = function(board,depth,maximizing) {
+        //need to check who won or if it was a tie so I can set a score to it.
+        //maximizer will always be the letter o
+        if (checkWin('O')) {return 10+depth}
+        if (depth == 0) {return}
+        if (checkWin('X')) {return -10-depth}
+        console.log(depth)
+        //need to copy new board after placing move
+        const copy = structuredClone(board);
+        const moves = getMoves(copy);
+        if (maximizing) {
+        best = Infinity;
+        moves.forEach((move) => {
+            copy[move[0]][move[1]] = "O"
+            value = minimax(copy,depth-1,false);
+            best = Math.max(value,best)
+            return best
+        })} else {
+        best = -Infinity;
+        moves.forEach((move) => {
+            //need to copy new board after placing move
+            copy[move[0]][move[1]] = 'O';
+            value = minimax(copy,depth-1,true);
+            best = Math.max(value,best)
+            return best
+        })};
+    };
+  
+    return {board,minimax,checkWin,play,reset,full,getMoves}
+};
 
-//     if current board state is a terminal state :
-//         return value of the board
+let board = game([
+    ['X','','X'],
+    ['','O',''],
+    ['O','','X']
+]);
 
-//     if isMaximizingPlayer :
-//         bestVal = -INFINITY
-//         for each move in board :
-//             value = minimax(board, depth+1, false)
-//             bestVal = max( bestVal, value)
-//         return bestVal
-
-//     else :
-//         bestVal = +INFINITY
-//         for each move in board :
-//             value = minimax(board, depth+1, true)
-//             bestVal = min( bestVal, value)
-//         return bestVal
